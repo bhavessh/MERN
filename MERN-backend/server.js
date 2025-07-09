64a12c61732e9ff34d02dba5b723c8a79066c078
bhavessh <jbhavesh.in@gmail.com>
2025-07-09 09:56:45 +0530

    update

---
+++ MERN-backend/server.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import express from "express";
+import userRouter from "./routes/userRoutes.js"
+const app = express();
+mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
+});
+app.use(express.json());
+app.use("/api/users", userRouter);
