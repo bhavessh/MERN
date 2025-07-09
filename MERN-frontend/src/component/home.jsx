3f876101470ba79f2b135fa4d9f0fac2cc2049fc
bhavessh <jbhavesh.in@gmail.com>
2025-07-09 10:59:08 +0530

    .updat

---
+++ MERN-frontend/src/component/home.jsx
@@ -0,0 +1,19 @@
+
+// export default function Home({ age }) {
+//   let name = "John";
+//   return (
+//     <>
+//       <div>
+//         Hello {name}. You are {age} years old.
+//       </div>
+//       <p>This is a paragraph</p>
+//     </>
+//   );
+// }
+/
+
+// export default function Home({age}){
+//   return age>18 && <h2>welcome</h2>
+// }
+
+
