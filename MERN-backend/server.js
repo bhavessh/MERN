16d9dc0008c4e52520a575e67d710da7270084b9
bhavessh <jbhavesh.in@gmail.com>
2025-07-10 11:44:59 +0530

    updated

--- MERN-backend/server.js
+++ MERN-backend/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors"
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors())
 // app.use(express.static("public"));
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
-// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
-//   app.listen(8080, () => {
-//     console.log("Server started");
-//   });
-// });
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
+});
 
 mongoose
   .connect(
